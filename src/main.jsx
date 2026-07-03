0992b6821155321229989fb0db8fbef74b8962bf
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-03 20:47:28 +0530

    Added icon imports to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,18 @@
 import React, { useMemo, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import {
+  ArrowLeft,
+  Bell,
+  Check,
+  ChevronRight,
+  HeartHandshake,
+  LogOut,
+  Mail,
+  MapPin,
+  NotebookPen,
+  Search,
+  Send,
+  ShieldCheck,
+  Sparkles,
+  UserRound,
+  Users,
